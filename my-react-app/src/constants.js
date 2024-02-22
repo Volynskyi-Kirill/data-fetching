7f005421a32ac482a20cf0b614e3c490d9c0cd79
Kirill108 <kirill108bol@gmail.com>
2024-02-22 21:18:23 +0100

    add remote url

--- my-react-app/src/constants.js
+++ my-react-app/src/constants.js
@@ -2,4 +2,4 @@ export const minInputValue = 0;
 export const maxInputValue = 100;
 export const defaultInputValue = "";
 
-export const URL = "http://localhost:3000/";
+export const URL = "https://data-fetching.onrender.com/";
